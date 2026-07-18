5d5ded5b44e878f36be6f41d17098b4071f438ac
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-18 20:21:26 +0500

    sidebar

--- src/components/Layout/SideBar.tsx
+++ src/components/Layout/SideBar.tsx
@@ -1,34 +1,44 @@
-import {ChartNoAxesGantt, LayoutDashboard} from "lucide-react"
+import { ChartNoAxesGantt, LayoutDashboard } from "lucide-react";
 import type { NavItems } from "../types/types";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+import { cn } from "@/lib/utils";
 
 const SideBar = () => {
-
-    const navigationItems: NavItems[] = [
-      {
-        icon: <LayoutDashboard />,
-        link: "/adminpanel",
-        id: 1,
-      },
-      {
-        icon: <ChartNoAxesGantt />,
-        link: "/products",
-        id: 2,
-      },
-    ];
+  const location = useLocation();
+  console.log(location);
+  const navigationItems: NavItems[] = [
+    {
+      icon: <LayoutDashboard />,
+      link: "/adminpanel",
+      id: 1,
+    },
+    {
+      icon: <ChartNoAxesGantt />,
+      link: "/products",
+      id: 2,
+    },
+  ];
 
   return (
     <div className="max-w-25 w-fit min-h-screen bg-foreground">
-      <div className=" p-7 flex justify-center items-center flex-col gap-5">
+      <div className=" py-7 flex justify-center items-center flex-col gap-5">
         {navigationItems.map((el) => (
           <Link to={el.link} key={el.id}>
-            {" "}
-            <div className="text-primary-foreground hover:text-primary-foreground/50 duration-300">{el.icon}</div>
+            <div
+              className={cn(
+                "hover:text-primary-foreground/40 ",
+                el.link == location.pathname
+                  ? "text-primary-foreground border-l-4  px-5 "
+                  : "text-primary-foreground/70 duration-300",
+              )}
+            >
+              {el.icon}
+            </div>
           </Link>
         ))}
       </div>
     </div>
   );
-}
+};
 
-export default SideBar
+export default SideBar;
