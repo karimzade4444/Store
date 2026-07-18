853150cf6ffa7583675ca6fa3b13ab497e86531b
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-18 19:35:14 +0500

    File

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-const App = () => {
-  return <div>
+import { createBrowserRouter } from "react-router";
+import SideBar from "./components/Layout/SideBar";
+import MainLayout from "./components/Layout/MainLayout";
+import AdminPanel from "./components/pages/adminPanel/adminPanel";
+import Products from "./components/pages/products/Products";
 
-    
-  </div>;
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/adminpanel",
+          element: <AdminPanel />,
+        },
+        {
+          index: true,
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+  ]);
+  return <div></div>;
 };
 
 export default App;
---
+++ src/components/pages/adminPanel/adminPanel.tsx
@@ -0,0 +1,9 @@
+
+
+const AdminPanel = () => {
+  return (
+    <div>adminPanel</div>
+  )
+}
+
+export default AdminPanel
