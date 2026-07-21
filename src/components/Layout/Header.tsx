1a3221f9783b3d634789c68c48537ff9e199e8df
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-22 00:08:23 +0500

    Profile

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -11,23 +11,23 @@ const Header = () => {
         <CustomInput
           start={<Search className="size-10 pr-3 " color="black" />}
           placeholder="Search"
-          className="bg-primary-foreground py-5 w-120"
+          className="bg-primary-foreground py-5 w-120 shadow"
         />
         <div className="flex justify-center items-center gap-5">
-          <Button>
-            <Bell />
+          <Button variant="outline" className="cursor-pointer size-12 shadow">
+            <Bell className="size-5 "/>
           </Button>
-          <Button>
-            <Settings />
+          <Button variant="outline" className="cursor-pointer size-12 shadow">
+            <Settings className="size-5"/>
           </Button>
           <div className="flex justify-center items-center gap-2">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQzFWxj0S7AVqUL5nhSDNmjWq5tv_zm1NdvaBUkM3SJmefg9llPLGi2Dg&s=10"
               alt="fdf"
-              className="w-12 h-12 rounded-xl"
+              className="w-12 h-12 rounded-xl shadow"
             />
             <div>
-              <p className=" font-black">Karimzoda</p>
+              <p className=" font-black">Karimzoda Mustafo</p>
               <p className=" text-black/50">Admin</p>
             </div>
           </div>
