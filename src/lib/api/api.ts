8cace39753b8b9c0a918daf197b707ee3d01cc76
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-22 00:20:16 +0500

    api

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import type { IGetProducts } from "../types/types";
 
 const api = "https://69e5ff70ce4e908a155ec5a1.mockapi.io/mmj";
 
-export const getProducts = async (search:string): Promise<>=>{
+export const getProducts = async (search:string): Promise<IGetProducts>=>{
     return(
         await axios.get(api, {
             params:search?{search}:{},
