2cb25080d43cd98e74d4fd9d1a8aefd058dae7a9
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-21 23:48:49 +0500

    Notification

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,17 +1,22 @@
-import { Search } from "lucide-react";
+import { Bell, Search } from "lucide-react";
 import CustomInput from "../custom/CustomInput";
+import { Button } from "../ui/button";
 
 
 
 const Header = () => {
   return (
     <div className="w-full max-h-25 h-fit bg-secondary border-b-2 p-5">
-      <CustomInput
-        start={<Search className="size-10 pr-3 " color="black"/>}
-        placeholder="Search"
-        className="bg-primary-foreground py-5"
-      />
-      
+      <div>
+        <CustomInput
+          start={<Search className="size-10 pr-3 " color="black" />}
+          placeholder="Search"
+          className="bg-primary-foreground py-5 w-120"
+        />
+        <Button>
+          <Bell />
+        </Button>
+      </div>
     </div>
   );
 }
