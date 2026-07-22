70e34c313745216b5f78746adf9d56ba504be781
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-23 00:46:19 +0500

    DataTable

--- src/components/pages/adminPanel/adminPanel.tsx
+++ src/components/pages/adminPanel/adminPanel.tsx
@@ -1,9 +1,68 @@
-
+import { useStore } from "@/components/store/store";
+import { Button } from "@/components/ui/button";
+import { getProducts } from "@/lib/api/api";
+import { useQuery } from "@tanstack/react-query";
+import { DataTable } from "mantine-datatable";
 
 const AdminPanel = () => {
-  return (
-    <div>adminPanel</div>
-  )
-}
+  const { data, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: () => getProducts(""),
+  });
+  const { search } = useStore();
 
-export default AdminPanel
+  const filtered = data?.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase()),
+  );
+  return (
+    <div className="p-10">
+      <DataTable
+        records={filtered ?? []}
+        columns={[
+          {
+            accessor: "id",
+            title: "ID",
+          },
+          {
+            accessor: "img",
+            title: "Image",
+            render: (record) => (
+              <img
+                src={record.img}
+                className="w-14 h-14 object-cover rounded"
+              />
+            ),
+          },
+          {
+            accessor: "name",
+            title: "Product",
+          },
+          {
+            accessor: "price",
+            title: "Price",
+            render: (record) => `$${record.price.toFixed(2)}`,
+          },
+          {
+            accessor: "stock",
+            title: "Stock",
+          },
+          {
+            accessor: "actions",
+            title: "Actions",
+            render: (record) => (
+              <div className="flex gap-2 ">
+                <Button>Edit</Button>
+
+                <Button variant="destructive" className="cursor-pointer">
+                  Delete
+                </Button>
+              </div>
+            ),
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default AdminPanel;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "mantine-datatable/styles.css";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const client = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
-      <App />
+      <MantineProvider>
+        <App />
+      </MantineProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
