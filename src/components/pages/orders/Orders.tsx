86e2a6af1b4943a3c3483f6f1cd5934772721d02
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-23 00:27:31 +0500

    Summary

--- src/components/pages/orders/Orders.tsx
+++ src/components/pages/orders/Orders.tsx
@@ -5,53 +5,97 @@ import { Button } from "@/components/ui/button";
 
 const Orders = () => {
   const { cart, increment, decrement, removeFromCart } = useStore();
-  
+  const total = cart.reduce(
+    (sum, item) => sum + Math.max(0,(item.price - item.sale) * item.quantity),
+    0,
+  );
+  const subtotal = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
+  const discount = cart.reduce(
+    (sum, item) => sum + item.sale * item.quantity,
+    0,
+  );
 
   return (
     <>
       <div>
         <p className="p-5 text-3xl font-bold">Order details</p>
-        <div className=" p-5 flex flex-col gap-5">
-          {cart.map((item) => (
-            <div
-              key={item.id}
-              className="max-w-[50%] h-30 bg-border flex items-start justify-between gap-5 border rounded-2xl shadow"
-            >
-              <div>
-                <img src={item.img} alt="" className="w-42 rounded-2xl" />
-              </div>
-              <div className="p-5 leading-12">
-                <p className=" text-2xl">{item.name}</p>
-                <div className="w-40 flex  items-center gap-5">
-                  <p>$ {Math.max(0, (item.price - item.sale)*item.quantity).toFixed(2)}</p>
-                  <p className="text-black/50 relative">
-                    $ {(item.price*item.quantity).toFixed(2)}
-                    <div className="w-full h-0.5 rounded-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/50 z-0"></div>
-                  </p>
+        <div className=" flex justify-start items-start gap-50">
+          <div className=" p-5 flex flex-col gap-5 w-[50%]">
+            {cart.map((item) => (
+              <div
+                key={item.id}
+                className="w-full h-30 bg-border flex items-start justify-between gap-5 border rounded-2xl shadow"
+              >
+                <div>
+                  <img src={item.img} alt="" className="w-42 rounded-2xl" />
                 </div>
-              </div>
-              <div className="flex items-center justify-between mt-10 w-50 h-12 rounded-lg border px-4 py-2 bg-secondary text-primary">
-                <button
-                  className="cursor-pointer text-2xl hover:opacity-30 duration-300"
-                  onClick={() => decrement(item.id)}
-                >
-                  -
-                </button>
+                <div className="p-5 leading-12">
+                  <p className=" text-2xl">{item.name}</p>
+                  <div className="w-40 flex  items-center gap-5">
+                    <p>
+                      ${" "}
+                      {Math.max(
+                        0,
+                        (item.price - item.sale) * item.quantity,
+                      ).toFixed(2)}
+                    </p>
+                    <p className="text-black/50 relative">
+                      $ {(item.price * item.quantity).toFixed(2)}
+                      <div className="w-full h-0.5 rounded-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/50 z-0"></div>
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-between mt-10 w-50 h-12 rounded-lg border px-4 py-2 bg-secondary text-primary">
+                  <button
+                    className="cursor-pointer text-2xl hover:opacity-30 duration-300"
+                    onClick={() => decrement(item.id)}
+                  >
+                    -
+                  </button>
 
-                <span>{item.quantity}</span>
+                  <span>{item.quantity}</span>
 
-                <button
-                  className="cursor-pointer text-2xl hover:opacity-30 duration-300"
-                  onClick={() => increment(item.id)}
+                  <button
+                    className="cursor-pointer text-2xl hover:opacity-30 duration-300"
+                    onClick={() => increment(item.id)}
+                  >
+                    +
+                  </button>
+                </div>
+                <Button
+                  variant="destructive"
+                  className="cursor-pointer"
+                  onClick={() => removeFromCart(item.id)}
                 >
-                  +
-                </button>
+                  X
+                </Button>
               </div>
-              <Button variant="destructive"  className="cursor-pointer" onClick={()=>removeFromCart(item.id)}>X</Button>
+            ))}
+          </div>
+          <div className="w-[30%] max-h-60 bg-border mt-5 rounded-2xl shadow border p-5">
+            <h2 className="text-2xl font-bold">Summary</h2>
+
+            <div className="mt-5 flex justify-between">
+              <span>Subtotal</span>
+              <span>${subtotal.toFixed(2)}</span>
             </div>
-          ))}
+
+            <div className="mt-3 flex justify-between">
+              <span>Discount</span>
+              <span>-${discount.toFixed(2)}</span>
+            </div>
+
+            <hr className="my-5" />
+
+            <div className="flex justify-between text-xl font-bold">
+              <span>Total</span>
+              <span>${total.toFixed(2)}</span>
+            </div>
+          </div>
         </div>
-        <div></div>
       </div>
     </>
   );
